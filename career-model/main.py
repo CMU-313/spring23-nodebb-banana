641a44d77a9cf06bb99a59e636b9ce286d4b9b35
Ire Alarape <ialarape@andrew.cmu.edu>
2023-04-11 19:03:48 -0400

    Lint fixes

--- career-model/main.py
+++ career-model/main.py
@@ -9,8 +9,8 @@ app = FastAPI()
 def read_root():
     return {"Employability Predictor API"}
 
-@app.post("/predict_student/")
-async def predict_students(studentInfo : Student):
+@app.post("/")
+async def predict_student(studentInfo : Student):
     # Run the ML model on input
     prediction = predict(studentInfo)
     # Cast type to int because of type of resonse (numpy int.64)
